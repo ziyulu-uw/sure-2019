0f2b3a768dcab7379066ac9fcb499ab15cbe146e
Ziyu Lu <zl1546@nyu.edu>
2019-06-19 11:45:10 +0800

    documented

--- Ziyu_code/kalman_filter_GD.py
+++ Ziyu_code/kalman_filter_GD.py
@@ -1,18 +1,28 @@
+# Author: Ziyu Lu
+# Email: zl1546@nyu.edu
+# Date: June 2019
+# Description: This program finds the optimal Kalman gain using gradient descent and stochastic gradient descent \
+# This program also includes testing code for gradient using finite difference approximation.
+
+
 import math
 import cmath
 import numpy as np
 import matplotlib.pyplot as plt
 
 # constants
-omega = 0.5  # omega = k/m
-mu = 0.01    # mu  = gamma/m
+m = 1   # mass of the object
+k = 0.5  # spring constant
+gamma = 0.01  # friction coefficient
+omega = k/m
+mu = gamma/m
 w = math.sqrt(omega - 0.25*mu**2)
-N = 200  # number of time steps in one simulation
+N = 100  # number of time steps in one simulation
 dt = 0.05  # step size in one simulation
 sigma = 0.1  # noise coefficient in SDE
-var_v = 0.03  # measurement noise variance
-X0 = np.array([[0.0],[1.0]])
-C = np.array([0.0, 1.0], ndmin=2)
+var_v = 0.05  # observation noise variance
+X0 = np.array([[1.0],[0.0]])  # initial state (X = [x, v]^T)
+C = np.array([1.0, 0.0], ndmin=2) # observation matrix
 
 
 lambda1 = complex(-0.5*mu,w)
@@ -32,7 +42,7 @@ e1 = cmath.exp(2*lambda1*dt)
 e2 = cmath.exp(2*lambda2*dt)
 e3 = cmath.exp((lambda1 + lambda2)*dt)
 R11 = 0.5*(e1-1)/lambda1 + 0.5*(e2-1)/lambda2 - 2*(e3-1)/(lambda1 + lambda2)
-R12 = -0.5*(e1-1) - 0.5*(e2-1) + 2*(e3-1)
+R12 = 0.5*(e1-1) + 0.5*(e2-1) - (e3-1)
 R21 = R12
 R22 = lambda1*0.5*(e1-1) + lambda2*0.5*(e2-1) - 2*lambda1*lambda2*(e3-1)/(lambda1 + lambda2)
 R = np.array([[R11, R12], [R21, R22]])
@@ -40,10 +50,12 @@ R = np.multiply(R, (sigma/(lambda2-lambda1))**2)  # R turns out to be real
 R = R.real
 
 
-def compute_gradient(K):
+def compute_gradient(K, z):
+    # K -- Kalman gain, z -- random seed
     # computes dF/dK, where F = 1/2N * \sum_{n=1}^N (X_hat_n-X_n)^2
     # returns dF/dK, F
 
+    np.random.seed(z)  # set random seed
     X = X0  # initial state
     X_hat = X0  # initial state estimate
     dX_hat = np.zeros((2,2))  # initial gradient dX_hat/dK
@@ -51,6 +63,7 @@ def compute_gradient(K):
     sum_errors = 0
 
     for n in range(N):
+
         Z_hat = C @ A @ X_hat  # predicted observation
         W = np.random.multivariate_normal([0,0], R)  # gaussian system noise with mean 0 covariance R
         W = np.array(W, ndmin=2)
@@ -64,21 +77,23 @@ def compute_gradient(K):
         dX_hat = A @ dX_hat + np.diag(diag) - K @ C @ A @ dX_hat
         sum_gradients += np.transpose(X_hat - X) @ dX_hat
         error = X_hat - X
-        sum_errors += error[0][0]**2 + error[1][0]**2
+        sum_errors += (error[0][0]**2 + error[1][0]**2)
 
     return sum_gradients/N, sum_errors/(2*N)
 
 
-def gradient_descent(n, alpha):
-    # n -- number of gradient steps, alpha -- step size
+def stochastic_gradient_descent(n, alpha, z):
+    # n -- number of gradient steps, alpha -- step size, z -- random seed
     # performs gradient descent using dF/dK as gradient
     # returns K, a list of F at each gradient step, a list of dF/dK at each gradient step
 
-    K = np.array([[1.0], [1.0]])  # initialize K
+    # delta_K = 0.0001
+    K = np.array([[-1], [-1]])  # initialize K
     err_L = []
     grad_L = []
     for i in range(n):
-        grad, err = compute_gradient(K)
+        grad, err = compute_gradient(K, z)
+        # grad, err = finite_diff_approx(K, delta_K, z)
         err_L.append(err)
         grad_L.append(grad)
         K = K - alpha*np.transpose(grad)
@@ -87,8 +102,9 @@ def gradient_descent(n, alpha):
     return K, err_L, grad_L
 
 
-def expected_gradient_descent(n, alpha, m):
-    # n -- number of gradient steps, alpha -- step size, m -- number of simulations to compute expectation
+def gradient_descent(n, alpha, m, z):
+    # n -- number of gradient steps, alpha -- step size, m -- number of simulations to compute expectation, \
+    # z -- random seed
     # performs gradient descent using E[dF/dK] as gradient
     # returns K, a list of E[F] at each gradient step, a list of E[dF/dK] at each gradient step
 
@@ -99,7 +115,7 @@ def expected_gradient_descent(n, alpha, m):
         sum_grad = np.zeros((1,2))
         sum_err = 0
         for j in range(m):
-            grad, err = compute_gradient(K)
+            grad, err = compute_gradient(K, z)
             sum_grad += grad
             sum_err += err
         exp_grad = sum_grad/m  # E[dF/dK] is approximated by 1/m * \sum_{i=1}^N dF_i/dK
@@ -112,15 +128,15 @@ def expected_gradient_descent(n, alpha, m):
     return K, err_L, grad_L
 
 
-def Gradient_descent(n,alpha):
-    # calls gradient_descent(n, alpha) and plots F vs n, log(dF/dK) vs n
+def Stochastic_gradient_descent(n,alpha, z):
+    # calls stochastic_gradient_descent(n, alpha, z) and plots F vs n, log(dF/dK) vs n
 
-    K, err_L, grad_L = gradient_descent(n, alpha)
+    K, err_L, grad_L = stochastic_gradient_descent(n, alpha, z)
     print(K)
     print(err_L)
     x = [i for i in range(n)]
     plt.plot(x, err_L)
-    plt.title("Gradient descent with {} steps and step size {}".format(str(n),str(alpha)))
+    plt.title("Stochastic gradient descent with {} steps and step size {}".format(str(n),str(alpha)))
     plt.xlabel("number of gradient descent steps")
     plt.ylabel("mean squared error of one simulation")
     plt.show()
@@ -131,6 +147,7 @@ def Gradient_descent(n,alpha):
     log_grad2_L = [math.log10(abs(grad2_L[i])) for i in range(n)]
     plt.plot(x, log_grad1_L, label='grad1')
     plt.plot(x, log_grad2_L, label='grad2')
+    plt.title("Stochastic gradient descent with {} steps and step size {}".format(str(n), str(alpha)))
     plt.xlabel("number of gradient descent steps")
     plt.ylabel("log10(gradient)")
     plt.legend()
@@ -138,10 +155,10 @@ def Gradient_descent(n,alpha):
 
 
 
-def Expected_gradient_descent(n, alpha, m):
-    # calls expected_gradient_descent(n, alpha, m) and plots E[F] vs n, log(E[dF/dK]) vs n
+def Gradient_descent(n, alpha, m, z):
+    # calls gradient_descent(n, alpha, m, z) and plots E[F] vs n, log(E[dF/dK]) vs n
 
-    K, err_L, grad_L = expected_gradient_descent(n, alpha, m)
+    K, err_L, grad_L = gradient_descent(n, alpha, m, z)
     print(K)
     print(err_L)
     x = [i for i in range(n)]
@@ -157,12 +174,97 @@ def Expected_gradient_descent(n, alpha, m):
     log_grad2_L = [math.log10(abs(grad2_L[i])) for i in range(n)]
     plt.plot(x, log_grad1_L, label='grad1')
     plt.plot(x, log_grad2_L, label='grad2')
+    plt.title("Expected gradient descent with {} steps and step size {}".format(str(n), str(alpha)))
     plt.xlabel("number of gradient descent steps")
     plt.ylabel("log10(gradient)")
     plt.legend()
     plt.show()
 
 
-# Gradient_descent(500,0.001)
-# Expected_gradient_descent(500, 0.001, 10)
+# Testing code
+def finite_diff_approx(K, delta_K, z):
+    # K -- Kalman gain, delta_K -- difference in finite difference approximation, z -- random seed
+    # performs first order finite difference approximation of dF/dK: dF/dK = (F(K+delta_K) - F(K))/delta_K
+    # returns dF/dK, and a finite difference approximation of dF/dK
+
+    grad_approx = np.array([[0.0, 0.0]])
+
+    # compute F(K)
+    np.random.seed(z)  # set random seed
+    X = X0  # initial state
+    X_hat = X0  # initial state estimate
+    sum_errors = 0
+
+    for n in range(N):
+        Z_hat = C @ A @ X_hat  # predicted observation
+        W = np.random.multivariate_normal([0, 0], R)  # gaussian system noise with mean 0 covariance R
+        W = np.array(W, ndmin=2)
+        W = np.transpose(W)
+        X = A @ X + W  # state update
+        V = np.random.normal(0, var_v)  # gaussian observation noise with mean 0 variance var_v
+        Z = C @ X + V  # observation
+        X_hat = A @ X_hat + K * (Z - Z_hat)  # state estimate
+        error = X_hat - X
+        sum_errors += error[0][0] ** 2 + error[1][0] ** 2
+
+    F = sum_errors/(2*N)
+
+    # compute F(K+delta_K)
+    for i in range(len(K)):
+
+        np.random.seed(z)  # set random seed
+        X = X0  # initial state
+        X_hat = X0  # initial state estimate
+        K[i][0] += delta_K
+        sum_errors = 0
+
+        for n in range(N):
+            Z_hat = C @ A @ X_hat  # predicted observation
+            W = np.random.multivariate_normal([0, 0], R)  # gaussian system noise with mean 0 covariance R
+            W = np.array(W, ndmin=2)
+            W = np.transpose(W)
+            X = A @ X + W  # state update
+            V = np.random.normal(0, var_v)  # gaussian observation noise with mean 0 variance var_v
+            Z = C @ X + V  # observation
+            X_hat = A @ X_hat + K * (Z - Z_hat)  # state estimate
+            error = X_hat - X
+            sum_errors += error[0][0] ** 2 + error[1][0] ** 2
+
+        F_ = sum_errors/(2*N)
+        grad_approx[0][i] = (F_ - F)/delta_K
+        K[i][0] -= delta_K
+
+    return grad_approx, F
+
+
+def check_order(K, delta_K, z, n):
+    # K -- Kalman gain, delta_K -- difference in finite difference approximation, \
+    # z -- random seed, n -- largest multiple of delta_K
+    # checks if the above first order approximation works properly \
+    # by plotting the norm of approximation error against the difference
+    # the plot should be linear
+
+    x_L = []
+    y_L = []
+    grad, err = compute_gradient(K, z)
+    print(grad)
+    for i in range(n):
+        delta_K_n = delta_K * (i+1)
+        grad_approx, F = finite_diff_approx(K, delta_K_n, z)
+        x_L.append(delta_K_n)
+        y_L.append(np.linalg.norm(grad_approx - grad))
+
+    # print(y_L)
+    plt.plot(x_L, y_L)
+    plt.show()
+
+
+# Testing
+# K = np.array([[2.0], [2.0]])
+# check_order(K, 0.00001,1,5)
+
+Stochastic_gradient_descent(500, 1e-5, 1)
+# Gradient_descent(500, 0.001, 10, 1)
+
+
 
