32b4d70d7a275b7bf5472240442ff08f7effa618
Xinyu Li <xinyuli@Xinyus-MacBook-Pro.local>
2019-07-11 22:14:02 +0800

    add BFGS and Nelder-Mead compare

--- Xinyu_code/LQR/main_find_G.py
+++ Xinyu_code/LQR/main_find_G.py
@@ -9,6 +9,17 @@ from cost import cost_fun
 from Model_tools import S_matrix
 from cost import cost_der
 from EigenTest import M_eval
-solution = minimize(cost_fun,(-0.01,-0.01),method = "BFGS",jac=cost_der)
-M_eval(np.reshape(solution.x,[1,2]),True)
-print(solution)
+
+"""find G use BFGS"""
+print("Find G with BFGS:")
+solution1 = minimize(cost_fun,(-0.01,-0.01),method = "BFGS",jac=cost_der)
+print(solution1)
+M_eval(np.reshape(solution1.x,[1,2]),True)
+
+print()
+"""find G use Nelder-Mead"""
+print("Find G with Nelder-Mead:")
+solution2 = minimize(cost_fun,(-0.01,-0.01),method = "Nelder-Mead")
+print(solution2)
+print()
+M_eval(np.reshape(solution2.x,[1,2]),True)
