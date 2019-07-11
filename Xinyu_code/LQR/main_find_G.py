4c1c934b5960c19a1804d34e6ec7ba8c5071b728
Xinyu Li <xinyuli@Xinyus-MacBook-Pro.local>
2019-07-11 12:21:47 +0800

    use QuasiNewton to find G

--- Xinyu_code/LQR/main_find_G.py
+++ Xinyu_code/LQR/main_find_G.py
@@ -1,4 +1,14 @@
 # Author: Xinyu Li
 # Email: xl1796@nyu.edu
 # Date: July 2019
-# Description: find G use Newton Method
+# Description: find G use qausi Newton Method
+
+import numpy as np
+from scipy.optimize import minimize
+from cost import cost_fun
+from Model_tools import S_matrix
+from cost import cost_der
+
+G = np.array([-0.05,-0.2]) #initial guess
+solution = minimize(cost_fun,(0.01,-0.01),method = "Nelder-Mead",jac=cost_der)
+print(solution)
