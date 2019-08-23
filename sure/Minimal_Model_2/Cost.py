f7902318a3b89e67a3990426807c5c06cf8a96ac
Ziyu Lu <zl1546@nyu.edu>
2019-08-23 15:19:15 +0800

    define new cost function

--- sure/Minimal_Model_2/Cost.py
+++ sure/Minimal_Model_2/Cost.py
@@ -1,19 +1,25 @@
-# Author: Xinyu Li
-# Email: xl1796@nyu.edu
+# Author: Xinyu Li, Ziyu Lu
+# Email: xl1796@nyu.edu, zl1546@nyu.edu
 # Date: August 2019
 # Description: The cost is calculated here
 
 import numpy as np
 
-def cost_computation(G, Gb, lam, vn_list, end_time):
+def cost_computation(G, vn_list):
     """a function to calculate the total cost from time 0 to t1
-    :param I_t   the value of insulin I(t) by simulation
-    :param Ib    basal insulin value
     :param G     simulation result of glucose function
-    :param Gb    basal glucose value
-    :param lam   coefficient that determine the ratio of two parts in cost function
-    :return total cost"""
+    :param vn_list:    a list of control exerted on the model
+    :return cost in the current prediction and control horizon"""
 
+    G_hat = np.zeros(len(G))
+    for i in range(len(G)):
+        if G[i] > 140:  # higher than the upper bound
+            G_hat[i] = G[i] - 140
+        elif G[i] < 80:  # lower than the lower bound
+            G_hat[i] = 80 - G[i]
+        else:  # within the euglycemic zone
+            G_hat[i] = 0
 
-    dt_G = end_time/(len(G) - 1)
-    return lam*np.sum(vn_list)  + np.sum((G-Gb)**2 * dt_G)
+    J = np.sum(G_hat**2) + np.sum(vn_list**2)*50
+
+    return J
